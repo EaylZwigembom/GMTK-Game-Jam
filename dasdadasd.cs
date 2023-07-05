6f04c87d862880cdd2c94dfc8c53dcb60d151205
EaylZwigembom <124462589+EaylZwigembom@users.noreply.github.com>
2023-07-05 17:57:45 +0000

    dsdsd

---
+++ dasdadasd.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Code2 : MonoBehaviour
+{
+    public string namee;
+    public int age = 12;
+    public Text msg;
+
+    public void OnClick()
+    {
+        if (age < 10)
+        {
+            msg.text = "You are too young";
+        }
+        else
+        {
+           w msg.text = "You can enter the game";
+        }
+    }
+
+    public void Click()
+    {
+        print(age++);
+    }
+}
